21de2e5787b1a308ce7365929bfa5377b9436a49
SagarDangal <35767614+SagarDangal@users.noreply.github.com>
2019-12-30 15:04:30 +0545

    repetition part

---
+++ sanzaal.sql
@@ -0,0 +1,97 @@
+SET SERVEROUTPUT ON;
+
+
+
+DECLARE
+    TYPE num_table IS TABLE OF VARCHAR2(20) INDEX BY VARCHAR2(20);
+    TYPE labell IS TABLE Of VARCHAR2(20) INDEX BY VARCHAR2(20);
+    TYPE exp IS TABLE Of VARCHAR2(20) INDEX BY VARCHAR2(20);
+    x NUMBER ;
+    type namesarray IS VARRAY(5) OF VARCHAR2(10);
+    name namesarray;
+    type numarray IS TABLE OF VARCHAR2(20); 
+    char VARCHAR2(20);
+    numss numarray := numarray();
+    n integer;
+    countt integer;
+    tables num_table := num_table();
+    nums2 numarray := numarray();
+    nums3 numarray := numarray();
+    labels labell := labell();
+    expr exp := exp();
+    patt VARCHAR2(20);
+    
+    
+FUNCTION getPattern(X IN NUMBER)RETURN VARCHAR2 IS patt VARCHAR2(20);
+    
+    BEGIN
+     name := namesarray('x','y','z','w');  
+   char := to_char(X);  
+   n := LENGTH(char);
+    --DBMS_OUTPUT.PUT_LINE(LPAD(char,10));
+    
+    for i in 1 .. n LOOP 
+        IF  not numss.exists(i)
+        THEN
+        numss.extend;
+        numss(i) := SUBSTR(char, i, 1);
+
+        --DBMS_OUTPUT.PUT_LINE(i);
+        
+        END IF;
+        
+    END LOOP;
+    --DBMS_OUTPUT.PUT_LINE(numss(2)); 
+    nums3 := numss;
+    nums2 := numss;
+    numss := numss MULTISET UNION DISTINCT nums2;
+    for i in numss.FIRST .. numss.LAST LOOP
+        --DBMS_OUTPUT.PUT_LINE(i);
+        n := REGEXP_COUNT(char,numss(i));
+         tables(numss(i)) := n;
+         if (n>=2) then
+            labels(numss(i)) := 'multiple';
+        else
+            labels(numss(i)) := 'single';
+            
+        end if;
+        --DBMS_OUTPUT.PUT_LINE(numss(i)||'----------------'||tables(numss(i))||'---------'||labels(numss(i)));
+        
+        
+    END LOOP;
+    countt := 1;
+    for i in numss.FIRST .. numss.LAST loop
+    if (labels(numss(i)) = 'multiple') then
+        --DBMS_OUTPUT.PUT_LINE(i);
+       expr(numss(i)) := name(countt);
+        countt := countt + 1;
+    else
+        expr(numss(i)) := 'o';
+    --DBMS_OUTPUT.PUT_LINE(i);
+    end if;
+    --DBMS_OUTPUT.PUT_LINE(labels(numss(i))||'---------- ------'||numss(i)||expr(numss(i)));
+    end loop;
+    patt := '';
+    --DBMS_OUTPUT.PUT_LINE(tables('2'));
+    for i in nums3.First .. nums3.LAST loop
+    --DBMS_OUTPUT.PUT_LINE(nums3(i)||'-----------------'||tables(nums3(i))||'--------------'||labels((nums3(i)))||'-------'||expr(nums3(i)));
+    if (expr(nums3(i)) != 'o') then
+     patt := concat(patt,expr(nums3(i)));
+     --DBMS_OUTPUT.PUT_LINE(nums3(i));
+    else 
+    patt := concat(patt,'*');
+    
+     end if;
+    
+    end loop;
+    --DBMS_OUTPUT.PUT_LINE(x||'----------------------'||patt);
+    RETURN patt;
+
+  END;  
+    
+    
+BEGIN 
+    x := 11223344;
+   patt := getPattern(x); 
+   DBMS_OUTPUT.PUT_LINE(x||'----------------------'||patt);
+END; 
